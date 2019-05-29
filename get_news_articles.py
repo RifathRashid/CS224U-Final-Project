21460ca9867317c9349bc1f288aea867e72f5b06
Rifath Rashid <rifath@Rifaths-MacBook-Pro.local>
2019-05-29 11:39:47 -0700

    Included jupyter notebook for scraping

--- get_news_articles.py
+++ get_news_articles.py
@@ -1,13 +1,28 @@
+
+'''
 from newsapi import NewsApiClient
 
-newsapi = NewsApiClient(api_key='af23605afdc54110bce1b726f110d18b')
+newsapi = NewsApiClient(api_key='secret_key')
 
 top_headlines = newsapi.get_everything(q='Lyft',
 									language='en',
-									from_param='2019-04-06')
+									from_param='2019-04-26')
 
 for article in top_headlines['articles']:
 	print("_____________________________________")
 	print(article['title'])
 	print(article['content'])
 	print("_____________________________________")
+'''
+
+
+import newspaper
+
+cnn_paper = newspaper.build('http://cnn.com')
+
+for article in cnn_paper.articles:
+	print(article.url)
+
+
+for category in cnn_paper.category_urls():
+	print(category)
